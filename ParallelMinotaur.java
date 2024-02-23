017031e3de69957258ea99c662604e4940d00e14
Jarod Davies <balancejackfireblade@gmail.com>
2024-02-23 15:40:23 -0500

    Further solving, lock seems to work
    Nice

--- ParallelMinotaur.java
+++ ParallelMinotaur.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class ParallelMinotaur extends Thread {
     private static final int numVictims = 100;
@@ -6,7 +7,7 @@ public class ParallelMinotaur extends Thread {
     public static void main(String [] args) throws InterruptedException {
         Victim [] victims = new Victim[numVictims];
 
-        Labyrinth labyrinth = new Labyrinth(true, 0); // Cupcake starts as being there
+        Labyrinth labyrinth = new Labyrinth(true, 0); // Cupcake starts as being there, initial victim doesn't matter
 
         for (int i = 0; i < numVictims; i++) {
             victims[i] = new Victim(labyrinth, i == 0, 0);
@@ -15,10 +16,39 @@ public class ParallelMinotaur extends Thread {
 
         Random rand = new Random();
 
-        int nextVictim;
-        do {
-            nextVictim = rand.nextInt(numVictims);
-        } while (!victims[nextVictim].doLabyrinth());
+        ReentrantLock labyrinthLock = new ReentrantLock();
 
+        int nextVictim;
+        boolean problemFinished = false;
+        while (!problemFinished) {
+            nextVictim = rand.nextInt(numVictims);
+            System.out.println("Victim " + (nextVictim + 1) + " has entered the labyrinth!");
+            if (labyrinthLock.tryLock()) {
+                try {
+                    problemFinished = victims[nextVictim].doLabyrinth(labyrinthLock);
+                } finally {
+                    labyrinthLock.unlock();
+                }
+            }
+        }
+
+        System.out.println("We believe everyone has been in the labyrinth!");
+
+        if (validateAnswer(victims)) {
+            System.out.println("Woohoo! We were right!");
+        } else {
+            System.out.println("Aw man! We were wrong.");
+        }
+
+    }
+
+    public static boolean validateAnswer(Victim[] victims) {
+        for (int i = 0; i < numVictims; i++) {
+            if (!victims[i].getVisited()) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
--- Victim.java
+++ Victim.java
@@ -1,11 +1,15 @@
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Victim extends Thread {
     private Labyrinth labyrinth;
+    private boolean visited = false; // We will be honest and say we didn't go yet.
 
     // One of our guys has tasked himself with eating every single cupcake.
     // It may be more logical to flip it and have him replenish all cupcakes,
     // which is easily doable, but this is way funnier.
     private boolean isHungryHungryHippo;
     private int timesToggled; // Helps keep track of both types of people
+
 
     public Victim(Labyrinth labyrinth, boolean isHungryHungryHippo, int timesToggled) {
         this.labyrinth = labyrinth;
@@ -14,7 +18,9 @@ public class Victim extends Thread {
     }
 
     // Does the labyrinth, returning true if we are ready to pronounce we have found the answer.
-    public boolean doLabyrinth() {
+    public boolean doLabyrinth(ReentrantLock labyrinthLock) {
+
+        this.visited = true;
 
         // Hungry hungry hippo has his own strategy. We need to now enact it.
         if (this.isHungryHungryHippo) {
@@ -32,5 +38,9 @@ public class Victim extends Thread {
 
         // I'm paranoid
         return false;
+    }
+
+    public boolean getVisited() {
+        return this.visited;
     }
 }
