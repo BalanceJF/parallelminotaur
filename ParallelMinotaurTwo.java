747634e8df41a432d0a59bd06049bff328e2ad60
Jarod Davies <balancejackfireblade@gmail.com>
2024-02-23 21:09:24 -0500

    Next pass over, seems to be working correctly

--- ParallelMinotaurTwo.java
+++ ParallelMinotaurTwo.java
@@ -1,10 +1,22 @@
-import java.lang.reflect.Array;
 import java.util.Random;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ParallelMinotaurTwo extends Thread {
     private static final int numVictims = 100;
-    private static final int numIterations = 1000; // Number of times people went in and saw the thing
+    private static final int numIterations = 100; // Number of times people went in and saw the thing
+    private static LinkedBlockingQueue<VictimTwo> victimQueue;
+    private static boolean done;
+
+    public void run() {
+        try {
+            while(!done) {
+                VictimTwo newVictim = victimQueue.take();
+                newVictim.doYoThang();
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
 
     public static void main(String [] args) throws InterruptedException {
         VictimTwo [] victims = new VictimTwo[numVictims];
@@ -12,51 +24,24 @@ public class ParallelMinotaurTwo extends Thread {
         Showroom room = new Showroom(true); // Room starts open
 
         for (int i = 0; i < numVictims; i++) {
-            victims[i] = new VictimTwo(room, false);
+            victims[i] = new VictimTwo(room, false, i + 1);
             victims[i].start();
         }
 
         Random rand = new Random();
 
-        LinkedBlockingQueue<VictimTwo> victimQueue = new LinkedBlockingQueue<>(MAX_PRIORITY); 
-
+        victimQueue = new LinkedBlockingQueue<>(MAX_PRIORITY); 
+        ParallelMinotaurTwo guyWhoWorksTheDoor = new ParallelMinotaurTwo();
+        guyWhoWorksTheDoor.start();
         for (int i = 0; i < numIterations; i++) {
             int nextVictim = rand.nextInt(numVictims);
             victimQueue.put(victims[nextVictim]);
         }
 
-
-        boolean problemFinished = false;
-        while (!problemFinished) {
-            nextVictim = rand.nextInt(numVictims);
-            System.out.println("Victim " + (nextVictim + 1) + " has entered the labyrinth!");
-            if (labyrinthLock.tryLock()) {
-                try {
-                    labyrinthLock.lock();
-                    problemFinished = victims[nextVictim].doLabyrinth(labyrinthLock);
-                    numEntries++;
-                } finally {
-                    labyrinthLock.unlock();
-                }
-            }
+        while(!victimQueue.isEmpty()) {
+            int Oh = 1;
         }
 
-        System.out.println("We believe everyone has been in the labyrinth! And it only took " + numEntries + " entries.");
-
-        if (validateAnswer(victims)) {
-            System.out.println("Woohoo! We were right!");
-        } else {
-            System.out.println("Aw man! We were wrong.");
-        }
-    }
-
-    public static boolean validateAnswer(Victim[] victims) {
-        for (int i = 0; i < numVictims; i++) {
-            if (!victims[i].getVisited()) {
-                return false;
-            }
-        }
-
-        return true;
+        done = true;
     }
 }
--- VictimTwo.java
+++ VictimTwo.java
@@ -1,12 +1,31 @@
+import java.util.Random;
+
 public class VictimTwo extends Thread {
     private Showroom room;
     private boolean visited; // We will be honest and say we didn't go yet.
+    private int id;
 
+    private static final int maxWaitingTime = 3000;
 
-    public VictimTwo(Showroom room, boolean visited) {
+    public VictimTwo(Showroom room, boolean visited, int id) {
         this.room = room;
         this.visited = visited;
+        this.id = id;
     }
 
+    // The problem doesn't really define what exactly someone "does" in the room, 
+    // so I'm just gonna have this thing wait for a bit as it admires the vase.
+    public void doYoThang() throws InterruptedException {
+        System.out.println("Victim " + id + " is entering the room!");
+        if (!visited) {
+            System.out.println("Ooooh, it's their first time! Lucky rascal!");
+            visited = true;
+        }
+        room.enter();
+        Random rand = new Random();
+        Thread.sleep(rand.nextInt(maxWaitingTime));
+        System.out.println("Victim " + id + " is exiting the room.");
+        room.exit();
+    }
     
 }
